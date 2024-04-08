48015efb4d2111be6e9b64f4471f9cd4a748656a
Igor Kvachenok <igor.kvachenok@prokube.ai>
2024-04-08 17:41:05 +0200

    create script to trigger KFP images workflow

---
+++ prokube-workflows/kfp_backend_images/build_backend_images.py
@@ -0,0 +1,22 @@
+import argparse
+import subprocess
+
+
+def main(commit_message: str, images: str) -> None:
+    dev = True
+    if dev:
+        images = "dev"
+
+    commit_command = ['git', 'commit', '-m', f"{commit_message} {images}"]
+    subprocess.run(commit_command, check=True)
+
+    push_command = ['git', 'push', 'origin', 'GCP-artifact-registry']
+    subprocess.run(push_command, check=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--images", choices=["all", "apiserver"])
+    parser.add_argument("--commit-message", default: "Trigger image build:")
+    args = parser.parse_args()
+    main(commit_message=args.commit_message, images=args.images)
